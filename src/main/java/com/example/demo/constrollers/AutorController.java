01b803230bb5ac85885dffcc0b240dcd45b05d02
Jefferson Zuanon <jefferson.zuanon@gpnet.com.br>
2022-08-16 12:37:43 -0300

    adicionado cors

--- src/main/java/com/example/demo/constrollers/AutorController.java
+++ src/main/java/com/example/demo/constrollers/AutorController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +29,7 @@ import com.example.demo.services.LivroService;
 
 @RestController
 @RequestMapping("/api/autores")
+@CrossOrigin(origins = "*")
 public class AutorController {
 
 	@Autowired
